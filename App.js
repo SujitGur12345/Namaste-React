88c025ad3d3aec93f81524b85147e82d3092c0a9
Sujit Walmiki <sujitwalmiki.nbnssoe.comp@gmail.com>
2025-11-27 00:49:37 +0530

    Nested Div using CreateElement

--- App.js
+++ App.js
@@ -1,7 +1,34 @@
-    //Create Element in React
-    const heading = React.createElement("h1",{id:"heading", xyz:"attribute2"},"Hello World From React..........")  //first parmater --> ElementType , 2nd param ---> attribute of tag , 3rd ---> 
+    /*
+    <div id="parent">
+        <div id="child">
+        <h1>I'm a Tag H1</h1>
+        </div>
+    
+    </div>
+     */
+    
+    const parent = React.createElement(
+        "div",
+        {id:"parent"}, 
+        React.createElement(
+            "div",
+            {id:"child"}, 
+            React.createElement("h1",{},"I'm a Tag H1")
+            )
+    );
+        
 
+
+    //Create Element in React
+    // const heading = React.createElement("h1",{id:"heading", xyz:"attribute2"},"Hello World From React..........")  //first parmater --> ElementType , 2nd param ---> attribute of tag , 3rd ---> 
+
+    // console.log(heading);
+    console.log(parent);
     //create Root
     const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    root.render(heading);
+    root.render(parent);
+
+    //Props ---> Children and Attribute
+    //Nested Div ---> createElement
+    //ReactElement(object) => 
