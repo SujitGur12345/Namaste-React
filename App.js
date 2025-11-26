2370f8644bdf5504f7a722f934f5ed4c5bbe7096
Sujit Walmiki <sujitwalmiki.nbnssoe.comp@gmail.com>
2025-11-27 00:11:02 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,7 @@
+    //Create Element in React
+    const heading = React.createElement("h1",{id:"heading"},"Hello World From React..........")  //first parmater --> ElementType , 2nd param ---> attribute of tag , 3rd ---> 
+
+    //create Root
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+
+    root.render(heading);
