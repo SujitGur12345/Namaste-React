33a0ff6bb3359ca4bf792491066a4250baa67c2b
Sujit Walmiki <sujitwalmiki.nbnssoe.comp@gmail.com>
2025-11-27 00:14:53 +0530

    Multiple Attribute option for react

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
     //Create Element in React
-    const heading = React.createElement("h1",{id:"heading"},"Hello World From React..........")  //first parmater --> ElementType , 2nd param ---> attribute of tag , 3rd ---> 
+    const heading = React.createElement("h1",{id:"heading", xyz:"attribute2"},"Hello World From React..........")  //first parmater --> ElementType , 2nd param ---> attribute of tag , 3rd ---> 
 
     //create Root
     const root = ReactDOM.createRoot(document.getElementById("root"));
